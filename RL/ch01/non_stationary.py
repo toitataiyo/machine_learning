ec2fc79ff88cef6505b6454fdc365f9d2e735dee
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-10 00:07:09 +0900

    correct non_stationary.py and adding result figure

--- RL/ch01/non_stationary.py
+++ RL/ch01/non_stationary.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from bandit import Agent
 
 class NonStatBandit:
     def __init__(self, arms=10):
@@ -16,16 +20,55 @@ class NonStatBandit:
 
 class AlphaAgent:
     def __init__(self, epsilon, alpha, actions=10):
-        def __init__(self, epsilon, alpha, actions=10):
-            self.epsilon = epsilon
-            self.Qs = np.zeros(actions)
-            self.alpha = alpha
+        self.epsilon = epsilon
+        self.Qs = np.zeros(actions)
+        self.alpha = alpha
 
-        def update(self, action, reward):
-            self.Qs[action] += (reward - self.Qs[action]) * self.alpha
+    def update(self, action, reward):
+        self.Qs[action] += (reward - self.Qs[action]) * self.alpha
 
-        def get_action(self):
-            if np.random.rand() < self.epsilon:
-                return np.random.randint(0, len(self.Qs))
-            return np.argmax(self.Qs)
+    def get_action(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
 
+runs = 200
+steps = 1000
+epsilon = 0.1
+alpha = 0.8
+agent_types = ["sample average", "alpha const update"]
+results = {}
+
+for agent_type in agent_types:
+    all_rates = np.zeros((runs, steps))
+
+    for run in range(runs):
+        if agent_type == "sample average":
+            agent = Agent(epsilon)
+        else:
+            agent = AlphaAgent(epsilon, alpha)
+        
+        bandit = NonStatBandit()
+        total_reward = 0
+        rates = []
+
+        for step in range (steps):
+            action = agent.get_action()
+            reward = bandit.play(action)
+            agent.update(action, reward)
+            total_reward += reward
+            rates.append(total_reward / (step + 1))
+
+        all_rates[run] = rates
+
+    avg_rates = np.average(all_rates, axis=0)
+    results[agent_type] = avg_rates
+
+plt.figure()
+plt.ylabel("Average Rates")
+plt.xlabel("Steps")
+for key, avg_rates in results.items():
+    plt.plot(avg_rates, label=key)
+plt.legend()
+
+plt.savefig("figure.png")
